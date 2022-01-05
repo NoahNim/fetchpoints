95e3fe070a174d3352ba591fd33959aeb0e5c7d7
NoahNim <nmedoff123@gmail.com>
2022-01-05 13:50:06 -0800

    do route planning comments and defining payer class planning comments

---
+++ index.js
@@ -0,0 +1,11 @@
+// Route planning
+// Have a route to GET payers and list their points
+// Such as router.get('payer/name')
+// Have a route to POST transactions that modify a payers points
+// Such as router.post('payer/name/:points/add')
+// router.post('payer/name/:points/subtract')
+
+
+// Payer planning
+// Payer will be a class
+// Each instance of a class will have payer: string, points: integer, timestamp: date
