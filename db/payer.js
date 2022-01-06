c8a3e6ffca204c6403ff43bcea0c9f99742b44e6
NoahNim <nmedoff123@gmail.com>
2022-01-06 10:05:52 -0800

    rethinking problem solution..

--- db/payer.js
+++ db/payer.js
@@ -1,22 +1,33 @@
-class Payer {
+class User {
     constructor(payer, points) {
         this.payer = payer;
-        this.points = points;
+        this.points = {
+            points: points,
+            
+        }
     }
 
-    subtractPoints(num) {
-        return this.points - num;
-    }
+    // this.points.reduce(function(a, b) { return a + b }, 0)
 }
 
-let david = new Payer('David', 5000);
-let corona = new Payer('Corona', 1000);
+let david = new User('David', 5000);
+let corona = new User('Corona', 1000);
 
 console.log(david)
 console.log(corona)
 
-class Transaction extends Payer {
-    constructor(timestamp) {
+class Transaction extends User {
+    constructor(payer, points, timestamp) {
+        super(payer, points);
         this.timestamp = timestamp;
     }
+    subtractPoints() {
+        return this.payer.points - this.points;
+    }
+    addPoints() {
+        
+    }
 }
+
+let buyCorona = new Transaction(david, corona.points, new Date());
+console.log(buyCorona);
