1a19d2b4f93b2e797bf34032aef1000e2b6bac7b
NoahNim <nmedoff123@gmail.com>
2022-01-06 12:28:42 -0800

    now have it so classes can modify each other

--- db/payer.js
+++ db/payer.js
@@ -1,10 +1,12 @@
 class User {
     constructor(payer, points) {
         this.payer = payer;
-        this.points = points
+        this.points = points;
     }
 
-    // this.points.reduce(function(a, b) { return a + b }, 0)
+    // subtractPoints(user, thePayer) {
+    //     return user.points = user.points - thePayer.points;
+    // }
 }
 
 let david = new User('David', 5000);
@@ -12,6 +14,8 @@ let corona = new User('Corona', 1000);
 
 console.log(david)
 console.log(corona)
+// david.subtractPoints(david, corona);
+// console.log(david);
 
 class Transaction extends User {
     constructor(payer, points, timestamp) {
@@ -19,12 +23,20 @@ class Transaction extends User {
         this.timestamp = timestamp;
     }
     subtractPoints() {
-        return this.payer.points - this.points;
+        if (this.payer.points > 0 && this.payer.points - this.points >= 0) {
+            return this.payer.points = this.payer.points - this.points;
+        }
     }
     addPoints() {
-        
+        return this.payer.points = this.payer.points + this.points
     }
 }
 
 let buyCorona = new Transaction(david, corona.points, new Date());
 console.log(buyCorona);
+buyCorona.addPoints();
+console.log(david);
+buyCorona.subtractPoints();
+console.log(david);
+buyCorona.subtractPoints();
+console.log(david);
