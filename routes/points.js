6617936bbc82ad24b9b3d111b7bb04f9aa503392
NoahNim <nmedoff123@gmail.com>
2022-01-07 14:37:09 -0800

    make a payment route, currently it doesn't work

--- routes/points.js
+++ routes/points.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const asyncHandler = require("express-async-handler");
-const UserList = require("../db/users")
+const UserList = require("../db/users");
+const Transaction = require("../db/classes");
 
 router.get('/users', asyncHandler(async (req, res) => {
     const users = await UserList;
@@ -18,7 +19,16 @@ router.get('/users/:id', asyncHandler(async (req, res) => {
 }))
 
 router.get('users/:userid/pay/company/:id', asyncHandler(async (req, res) => {
-    
+    const userId = req.params.userid;
+    const companyId = req.params.id;
+
+    const user = await UserList[userId];
+    const company = await UserList[companyId];
+
+    const payment = await new Transaction(user, company.points, new Date());
+    const currentTransaction = payment.subtractPoints();
+
+    return res.json(currentTransaction);
 }))
 
 module.exports = router;
