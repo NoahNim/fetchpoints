2144679dcaa0d225530e286ee3caa4b5d7c290a2
NoahNim <nmedoff123@gmail.com>
2022-01-07 11:44:49 -0800

    made get users route

--- routes/points.js
+++ routes/points.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const asyncHandler = require("express-async-handler");
 const User = require("./db/payer")
 const Transaction = require("./db/payer")
 
-router.get('/payer/name', {
-    
-})
+router.get('/users', asyncHandler(async (req, res) => {
+    const users = await User.findAll();
+
+    return res.json(users);
+}))
