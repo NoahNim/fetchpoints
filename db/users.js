13fea1f479ad89696074fb6fbfacc4637669f44e
NoahNim <nmedoff123@gmail.com>
2022-01-07 13:38:28 -0800

    add more users

--- db/users.js
+++ db/users.js
@@ -2,7 +2,10 @@ const User = require("./classes");
 const Transaction = require("./classes");
 
 const UserList = {
-    david: new User(1, "David", 5000)
+    1: new User(1, "David", 5000),
+    2: new User(2, "Andy", 9000),
+    3: new User(3, "Tiana", 9999),
+    4: new User(4, "Rob", 7000)
 }
 
 module.exports = UserList
