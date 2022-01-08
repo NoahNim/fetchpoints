a2ce093a73694221b9632a0f890469f168c550f7
NoahNim <nmedoff123@gmail.com>
2022-01-07 17:21:32 -0800

    cleaned up code

--- db/classes.js
+++ db/classes.js
@@ -5,9 +5,6 @@ class User {
     }
 }
 
-// let david = new User('David', 5000);
-// let corona = new User('Corona', 1000);
-
 class Transaction extends User {
     constructor(payer, points, timestamp) {
         super(payer, points);
@@ -26,14 +23,5 @@ class Transaction extends User {
         return { payer: this.payer.payer, points: this.points }
     }
 }
-
-// let buyCorona = new Transaction(david, corona.points, new Date());
-// console.log(buyCorona);
-// buyCorona.addPoints();
-// console.log(david);
-// buyCorona.subtractPoints();
-// console.log(david);
-// buyCorona.subtractPoints();
-// console.log(david);
 
 module.exports = { User, Transaction };
